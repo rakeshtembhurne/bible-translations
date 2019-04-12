bb4d1df54436e3507fec15b3ee23da27a5aa9f14
Rakesh Tembhurne <tj2point0@gmail.com>
2019-04-12 11:36:19 +0530

    added script to organize some data

---
+++ data-formatter.js
@@ -0,0 +1,112 @@
+require('dotenv').config()
+const util = require('util');
+const _ = require('lodash');
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+const queryAsync = util.promisify(connection.query.bind(connection));
+connection.connect();
+
+const log = data => console.log(JSON.stringify(data, null, 2));
+
+const list = {};
+const chapters = {
+    "Genesis": "GEN",
+    "Exodus": "EXO",
+    "Leviticus": "LEV",
+    "Numbers": "NUM",
+    "Deuteronomy": "DEU",
+    "Joshua": "JOS",
+    "Judges": "JDG",
+    "Ruth": "RUT",
+    "1 Samuel": "1SA",
+    "2 Samuel": "2SA",
+    "1 Kings": "1KI",
+    "2 Kings": "2KI",
+    "1 Chronicles": "1CH",
+    "2 Chronicles": "2CH",
+    "Ezra": "EZR",
+    "Nehemiah": "NEH",
+    "Esther": "EST",
+    "Job": "JOB",
+    "Psalm": "PSA",
+    "Proverbs": "PRO",
+    "Ecclesiastes": "ECC",
+    "Song of Songs": "SNG",
+    "Isaiah": "ISA",
+    "Jeremiah": "JER",
+    "Lamentations": "LAM",
+    "Ezekiel": "EZK",
+    "Daniel": "DAN",
+    "Hosea": "HOS",
+    "Joel": "JOL",
+    "Amos": "AMO",
+    "Obadiah": "OBA",
+    "Jonah": "JON",
+    "Micah": "MIC",
+    "Nahum": "NAM",
+    "Habakkuk": "HAB",
+    "Zephaniah": "ZEP",
+    "Haggai": "HAG",
+    "Zechariah": "ZEC",
+    "Malachi": "MAL",
+    "Matthew": "MAT",
+    "Mark": "MRK",
+    "Luke": "LUK",
+    "John": "JHN",
+    "Acts": "ACT",
+    "Romans": "ROM",
+    "1 Corinthians": "1CO",
+    "2 Corinthians": "2CO",
+    "Galatians": "GAL",
+    "Ephesians": "EPH",
+    "Philippians": "PHP",
+    "Colossians": "COL",
+    "1 Thessalonians": "1TH",
+    "2 Thessalonians": "2TH",
+    "1 Timothy": "1TI",
+    "2 Timothy": "2TI",
+    "Titus": "TIT",
+    "Philemon": "PHM",
+    "Hebrews": "HEB",
+    "James": "JAS",
+    "1 Peter": "1PE",
+    "2 Peter": "2PE",
+    "1 John": "1JN",
+    "2 John": "2JN",
+    "3 John": "3JN",
+    "Jude": "JUD",
+    "Revelation": "REV"
+};
+
+const getAllKeys = () => {
+    return new Promise((resolve, reject) => {
+        const query = {
+            sql: 'SELECT * FROM ??',
+            values: ['key_english']
+        }
+        connection.query(query, (error, products) => {
+            if (error) reject(error);
+            else resolve(products);
+        })
+    })
+}
+
+getAllKeys()
+.then(keys => {
+    _.each(keys, key => {
+        list[chapters[key.n]] = {
+            human: key.n,
+            short: chapters[key.n],
+            chapterId: key.b,
+            g: key.g
+        };
+    })
+    log(list);
+})
+.catch(console.error);
